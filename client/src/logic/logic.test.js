3bbbe0c9feb36ca12a54b70ad598721754e15d4e
laviniaeleanor <laviniaeleanor@outlook.com>
2018-05-08 15:38:36 +0200

    Logic fixed to return a random student also when the first random color chosen is not present in collection

--- client/src/logic/logic.test.js
+++ client/src/logic/logic.test.js
@@ -17,3 +17,13 @@ describe('getRandomStudent', () => {
     expect(getRandomStudent(test)).toBeDefined()
   })
 })
+
+describe('getRandomStudent', () => {
+    const test = [{name: 'a', evaluation : 'green'},{name: 'b', evaluation : 'yellow'},{name: 'c', evaluation : 'yellow'},{name: 'd', evaluation : 'yellow'},{name: 'e', evaluation : 'yellow'}]
+    const test1 = [{name: 'a', evaluation : 'green'},{name: 'b', evaluation : 'green'},{name: 'c', evaluation : 'green'},{name: 'd', evaluation : 'green'},{name: 'e', evaluation : 'green'}]
+
+    it('if the first color is not present in the collection, it randomizes until it returns a color that is present', () => {
+      expect(getRandomStudent(test)).toBeDefined()
+      expect(getRandomStudent(test1)).toBeDefined()
+    })
+})
